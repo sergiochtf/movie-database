8e305dcf1854bdfe483a20c02fd99ec32ce11d94
Sergio Castellano <sergio.castellano.herrera@gmail.com>
2021-10-16 16:11:05 +0100

    feat(global): Added navigation from bottombar

--- components/layout/BottomBar.vue
+++ components/layout/BottomBar.vue
@@ -6,25 +6,25 @@
     input-value
     :grow="$vuetify.breakpoint.mdAndUp"
   >
-    <v-btn value="popular">
+    <v-btn value="popular" to="/popular">
       <span>{{ $t('popular') }}</span>
 
       <v-icon>mdi-movie-filter</v-icon>
     </v-btn>
 
-    <v-btn value="playing">
+    <v-btn value="playing" to="/playing">
       <span>{{ $t('now-playing') }}</span>
 
       <v-icon>mdi-theater</v-icon>
     </v-btn>
 
-    <v-btn value="search">
+    <v-btn value="search" to="/search">
       <span>{{ $t('search') }}</span>
 
       <v-icon>mdi-movie-search</v-icon>
     </v-btn>
 
-    <v-btn value="favorite">
+    <v-btn value="favorite" to="/favorite">
       <span>{{ $t('favorite') }}</span>
 
       <v-icon>mdi-heart</v-icon>
@@ -33,20 +33,12 @@
 </template>
 
 <script>
-const ON_CLICK = 'on-click'
-
 export default {
   name: 'BottomBar',
   data() {
     return {
       value: 'search',
     }
-  },
-
-  watch: {
-    value(newValue) {
-      this.$emit(ON_CLICK, newValue)
-    },
   },
 }
 </script>
