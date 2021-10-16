553465f63e759ec0c5e68b9ba3b326ef25d2ffde
Sergio Castellano <sergio.castellano.herrera@gmail.com>
2021-10-16 17:26:28 +0100

    feat(movies): Listing mos popular movies

--- pages/popular/index.vue
+++ pages/popular/index.vue
@@ -1,3 +1,36 @@
 <template>
-  <span>HOLA</span>
+  <v-container grid-list-xs>
+    <h1 class="pb-6">{{ $t('most-popular') }}</h1>
+    <list-movies :movies="movies"></list-movies>
+    <more-btn @on-click-more="onClickMore"></more-btn>
+  </v-container>
 </template>
+<script>
+import { fetchPopular } from '~/utils/api.js'
+import List from '~/components/movies/List.vue'
+import MoreButton from '~/components/layout/MoreButton.vue'
+
+let page = 1
+
+export default {
+  components: {
+    'list-movies': List,
+    'more-btn': MoreButton,
+  },
+  async asyncData({ $axios, $config }) {
+    const movies = await fetchPopular($axios, page)
+    return { movies }
+  },
+
+  data() {
+    return { movies: [] }
+  },
+
+  methods: {
+    async onClickMore() {
+      const moreMovies = await fetchPopular(this.$axios, ++page)
+      this.movies = this.movies.concat(moreMovies)
+    },
+  },
+}
+</script>
